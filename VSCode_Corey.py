d4e7c7fd52b40dad6cbe72d9ddbd25e2c4531056
Yashwanth Muthuramalingam <yashwanth.yaju@gmail.com>
2020-08-31 15:43:45 +0200

    My first Git commit using VS Code (#1)

---
+++ VSCode_Corey.py
@@ -0,0 +1,18 @@
+import math
+import os
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Corey"))
+ 
+name = input("Your name mister? ")
+print("Hello Mr.", name)
